8a5280ba9429ce9cc4e28fe532488a180357c671
Pranav <impranavgarg@gmail.com>
2021-12-15 02:23:22 +0530

    first commit

---
+++ src/components/NewsItem.js
@@ -0,0 +1,27 @@
+
+import React, { Component } from "react";
+
+export class NewsItem extends Component {
+
+  render() {
+    let { title, description, imageUrl, newsUrl } = this.props;
+    return (
+      <div>
+        <div className="card bg-warning mt-2" >
+          <img src={imageUrl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title text-white">{title}</h5>
+            <p className="card-text text-muted" >
+              {description}
+            </p>
+            <a href={newsUrl} className="btn btn-sm btn-dark">
+              Read more
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NewsItem;
